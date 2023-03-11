6af51755dd70d38f33028cb0af977d7e785e20ac
Ryan Silvera <ryan.silvera@icloud.com>
2023-03-11 00:26:50 -0500

    Working on location logic

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("Your page is working");
+(()=>{const e=document.getElementById("submitBtn"),t=document.getElementById("searchbar");e.addEventListener("click",(e=>{let n=t.value;console.log(e),fetch(`https://api.openweathermap.org/data/2.5/weather?q=${n}&appid=2e4d099728cef11505f832bb292f954b`,{mode:"cors"}).then((function(e){return e.json()})).then((function(e){console.log(e)}))}))})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-alert('Your page is working')
+const submitBtn = document.getElementById('submitBtn')
+const searchbar = document.getElementById('searchbar')
+
+const getLocation = (location) => {
+    let userInput = searchbar.value
+    console.log(location)
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=2e4d099728cef11505f832bb292f954b`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+        })
+        .then(function(response) {
+        console.log(response);
+        });
+}
+
+submitBtn.addEventListener('click', getLocation)
+
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=2e4d099728cef11505f832bb292f954b', {mode: 'cors'})
+// .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(response) {
+//     console.log(response);
+//   });
