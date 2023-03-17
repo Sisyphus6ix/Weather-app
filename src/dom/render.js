77260093f5c2223368c6e471e2be53440897f37c
Ryan Silvera <ryan.silvera@icloud.com>
2023-03-17 00:09:34 -0400

    Displaying info on page

--- src/dom/render.js
+++ src/dom/render.js
@@ -2,8 +2,47 @@ let mainInfo = document.getElementsByClassName('main')[0]
 
 export const renderPage = (data) => {
    console.log(data)
+   clearPage()
+   displayWeather(data)
 }
 
-// const displayWeather = (data) => {
-//    console.log(data)
+// const getDate = () => {
+//    const date = new Date();
+
+//    let day = date.getDate();
+//    let month = date.getMonth() + 1;
+//    let year = date.getFullYear();
+
+//    let currentDate = `${day}-${month}-${year}`;
+//    console.log(currentDate);
 // }
+
+const displayWeather = (data) => {
+   let temp_cloudContainer = document.createElement('div')
+   let temperatures = document.createElement('div')
+   let clouds = document.createElement('div')
+   let todayTemp = document.createElement('p')
+   let highLowTempContainer = document.createElement('div')
+   let lowTemp = document.createElement('p')
+   let highTemp = document.createElement('p')
+   let cloudDetails = document.createElement('p')
+   
+   temp_cloudContainer.setAttribute('class', 'tempDiv')
+   temperatures.setAttribute('class', 'degrees')
+   clouds.setAttribute('class', 'cloudInfo')
+   todayTemp.innerText = `${data[0].temperature.temp}F°`
+   lowTemp.innerText = `${data[0].temperature.temp_min}`
+   highTemp.innerText = `${data[0].temperature.temp_max}`
+
+   temperatures.appendChild(todayTemp)
+   highLowTempContainer.appendChild(lowTemp)
+   highLowTempContainer.appendChild(highTemp)
+   temperatures.appendChild(highLowTempContainer)
+   temp_cloudContainer.appendChild(temperatures)
+   temp_cloudContainer.appendChild(clouds)
+   mainInfo.appendChild(temp_cloudContainer)
+}
+
+const clearPage = () => {
+   mainInfo.innerHTML = ''
+}
