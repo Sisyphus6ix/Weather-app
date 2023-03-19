38bc021e41d1494dada56ba4b6fb79072c1190ec
Ryan Silvera <ryan.silvera@icloud.com>
2023-03-18 22:06:59 -0400

    Work on toggle searchbar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { searchedLocation } from "./info"
 import { renderPage } from "./dom/render"
 
 const submitBtn = document.getElementById('submitBtn')
+const searchBtn = document.getElementById('searchBtn')
+const cancelBtn = document.getElementById('cancelBtn')
 
 
 submitBtn.addEventListener('click', () => {
@@ -10,4 +12,15 @@ submitBtn.addEventListener('click', () => {
     
     searchedLocation()
     .then(renderPage)
+})
+
+searchBtn.addEventListener('click', (e) => {
+    const searchbox = document.getElementsByClassName('searchbox')[0]
+    searchbox.classList.add('active')
+    console.log(e.target.src)
+})
+
+cancelBtn.addEventListener('click', () => {
+    const searchbox = document.getElementsByClassName('searchbox')[0]
+    searchbox.classList.remove('active')
 })
